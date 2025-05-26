9470b0e54d8576e9b48c7b357f7dfd15f5dcecb3
Kirill1one <Kirill2002ku@gmail.com>
2025-05-26 22:29:12 +0300

    Added form authorization

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import './Dashboard.css'
+
+export default function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Добро пожаловать в AnalyticsPro</h1>
+      <p>Здесь будет ваш дашборд</p>
+    </div>
+  )
+}
